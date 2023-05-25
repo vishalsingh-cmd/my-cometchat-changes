3bd955da096941804177ec021c8b8e4efa7990d1
João Gomes <joaogomes@significa.co>
2023-05-25 13:40:22 +0100

    chore: tailwind and vscode settings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="mt-10 text-xl text-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
