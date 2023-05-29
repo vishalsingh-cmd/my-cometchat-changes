dc886ec9254996c72c473e10ae90e7477c5c6006
Pedro Brandão <pedro@significa.co>
2023-05-29 14:59:24 +0100

    feat: colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1 class="mt-10 text-xl text-red-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{#each ['light', 'dark'] as theme}
+  <div data-theme={theme} class="bg-gray-1 p-6">
+    <h1 class="text-xl text-brand-9">Welcome to SvelteKit</h1>
+    <p class="text-gray-12">
+      Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
+    <p class="text-orange-9 dark:text-purple-9">
+      This text will be a different shade in dark theme
+    </p>
+  </div>
+{/each}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
+const createThemeScale = (scale: string, size = 12) => {
+  const themeScale: Record<number, string> = {};
+
+  for (let i = 1; i <= size; i++) {
+    themeScale[i] = `hsl(var(--color-${scale}-${i}) / <alpha-value>)`;
+  }
+
+  return themeScale;
+};
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: ['class', '[data-theme="dark"]'],
   theme: {
-    extend: {}
+    colors: {
+      brand: createThemeScale('brand'),
+      gray: createThemeScale('gray'),
+      orange: createThemeScale('orange'),
+      blue: createThemeScale('blue'),
+      purple: createThemeScale('purple'),
+      red: createThemeScale('red'),
+      green: createThemeScale('green')
+    }
   },
   plugins: []
 };
