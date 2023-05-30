07f5131299c0b0ad0040f0f22601c10c4490cc83
Pedro Brandão <pedro@significa.co>
2023-05-30 17:11:11 +0100

    feat: add script to compress icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+  import Icon from '$components/icon/icon.svelte';
+</script>
+
 <h1 class="mt-10 text-xl text-red-500">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Icon icon="activity" class="h-10 w-10 text-red-500" />
