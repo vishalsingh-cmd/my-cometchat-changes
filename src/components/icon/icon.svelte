457db677d2d92ea281d894fce8e68786d9f974a5
Pedro Brandão <pedro@significa.co>
2023-05-31 10:22:16 +0100

    feat: icons

--- src/components/icon/icon.svelte
+++ src/components/icon/icon.svelte
@@ -1,20 +1,14 @@
-<script context="module" lang="ts">
-  const files = import.meta.glob('./assets/*.svg', { as: 'raw', eager: true });
-
-  export const icons = Object.entries(files).reduce<Record<string, string>>((acc, [path, file]) => {
-    const name = path.replace('./assets/', '').replace('.svg', '');
-    acc[name] = file;
-    return acc;
-  }, {});
-</script>
-
 <script lang="ts">
   export let icon: string;
 </script>
 
-{#if icons[icon]}
-  <i data-icon={icon} aria-hidden="true" {...$$restProps}>
-    <!-- eslint-disable-next-line -->
-    {@html icons[icon]}
-  </i>
-{/if}
+<i data-icon={icon} aria-hidden="true" {...$$restProps}>
+  {#await import(`./assets/${icon}.svg?raw`)}
+    <div style="width: 24px; height: 24px;" />
+  {:then mod}
+    {#if mod && mod.default}
+      <!-- eslint-disable-next-line -->
+      {@html mod.default}
+    {/if}
+  {/await}
+</i>
