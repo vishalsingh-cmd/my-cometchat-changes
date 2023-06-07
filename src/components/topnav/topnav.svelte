888a61860524ad0bb881999c6888310ca490e760
Pedro Brandão <pedro@significa.co>
2023-06-07 11:45:10 +0100

    refactor: mouseover

--- src/components/topnav/topnav.svelte
+++ src/components/topnav/topnav.svelte
@@ -23,7 +23,7 @@
   >[];
 
   let activeIndex = -1;
-  $: isSolid = activeIndex > -1 || scrollY > 100;
+  $: isSolid = activeIndex > -1 || scrollY > 0;
 
   let scrollY = 0;
   let scrollYPrev = 0;
@@ -59,13 +59,14 @@
   >
     <div class="container mx-auto flex h-16 items-center justify-between px-4">
       <Logo />
-      <nav class="flex items-center gap-8">
+      <nav class="flex items-center" on:mouseleave={() => (activeIndex = -1)}>
         {#each data.links as item, i}
           {@const style =
-            'relative flex h-16 items-center overflow-hidden px-3 text-sm/none font-semibold tracking-widest transition-colors hover:text-brand-9'}
+            'relative flex h-16 items-center overflow-hidden px-5 text-sm/none font-semibold tracking-widest transition-colors hover:text-brand-9'}
           {#if item.component === 'link'}
             {@const { href, target, rel } = getAnchorFromCmsLink(item.link)}
             <a
+              on:mouseenter={() => (activeIndex = -1)}
               class={cn(style, $page.url.pathname === href && 'text-brand-9')}
               {href}
               {target}
@@ -76,13 +77,11 @@
           {:else}
             <button
               class={cn(style, i === activeIndex && 'text-brand-9')}
-              on:click={() => {
-                activeIndex = activeIndex === i ? -1 : i;
-              }}
+              on:mouseenter={() => (activeIndex = activeIndex === i ? -1 : i)}
             >
               <span
                 class={cn(
-                  'pointer-events-none absolute bottom-0 left-0 h-0 w-full bg-gradient-to-t from-brand-9/30 via-brand-9/10 via-30% to-brand-9/0 transition-all delay-0 duration-200 ease-in-out',
+                  'pointer-events-none absolute bottom-0 left-2 right-2 h-0 bg-gradient-to-t from-brand-9/30 via-brand-9/10 via-30% to-brand-9/0 transition-all delay-0 duration-200 ease-in-out',
                   'before:absolute before:bottom-0 before:left-0 before:h-0 before:w-full before:bg-brand-9 before:opacity-0 before:transition-all before:duration-200 before:ease-in-out',
                   i === activeIndex && 'h-6 delay-100 duration-500',
                   i === activeIndex && 'before:opacity-1 before:h-0.5'
@@ -92,6 +91,27 @@
             </button>
           {/if}
         {/each}
+        {#if data.links[activeIndex]}
+          {@const item = data.links[activeIndex]}
+          <div
+            in:slide={{ duration: 300, delay: 100 }}
+            out:slide={{ duration: 100 }}
+            class="fixed left-0 top-16 w-full border-b border-gray-12/5 bg-gray-3/98 backdrop-blur-xl"
+          >
+            {#if item.component === 'topnav-panel'}
+              {@const data = item.panel[0]}
+              {#if data.component === 'topnav-features-panel'}
+                <FeaturesPanel {data} />
+              {:else if data.component === 'topnav-solutions-panel'}
+                <SolutionsPanel {data} />
+              {:else if data.component === 'topnav-technologies-panel'}
+                <DevelopersPanel {data} />
+              {:else if data.component === 'topnav-resources-panel'}
+                <ResourcesPanel {data} {blogPosts} {customerStories} />
+              {/if}
+            {/if}
+          </div>
+        {/if}
       </nav>
 
       <div class="flex items-center gap-6">
@@ -111,25 +131,4 @@
       </div>
     </div>
   </header>
-  {#if data.links[activeIndex]}
-    {@const item = data.links[activeIndex]}
-    <div
-      in:slide={{ duration: 300, delay: 100 }}
-      out:slide={{ duration: 200 }}
-      class="border-b border-gray-12/5 bg-gray-3/98 backdrop-blur-xl"
-    >
-      {#if item.component === 'topnav-panel'}
-        {@const data = item.panel[0]}
-        {#if data.component === 'topnav-features-panel'}
-          <FeaturesPanel {data} />
-        {:else if data.component === 'topnav-solutions-panel'}
-          <SolutionsPanel {data} />
-        {:else if data.component === 'topnav-technologies-panel'}
-          <DevelopersPanel {data} />
-        {:else if data.component === 'topnav-resources-panel'}
-          <ResourcesPanel {data} {blogPosts} {customerStories} />
-        {/if}
-      {/if}
-    </div>
-  {/if}
 </div>
