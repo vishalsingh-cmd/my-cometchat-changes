28c4f9f8dd8de12c0c09d6c0634380a85418c2b6
João Gomes <joaogomes@significa.co>
2023-06-07 18:00:49 +0100

    feat(pre-footer): finish static version without dynamic starts

---
+++ src/stories/pre-footer.stories.ts
@@ -0,0 +1,15 @@
+import PreFooter from '$components/pre-footer/pre-footer.svelte';
+import type { Meta, StoryObj } from '@storybook/svelte';
+
+const meta = {
+  title: 'UI/PreFooter',
+  component: PreFooter,
+  argTypes: {}
+} satisfies Meta;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {}
+};
