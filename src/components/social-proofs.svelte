c9c678b4a59573572a34b89fa77acbafc5ca935a
João Gomes <joaogomes@significa.co>
2023-06-09 16:34:05 +0100

    chore(social-proofs): align items on center

--- src/components/social-proofs.svelte
+++ src/components/social-proofs.svelte
@@ -13,7 +13,7 @@
   class="flex flex-col items-center justify-center gap-8 bg-gray-1 pb-20 pt-16"
 >
   <h1 class="text-lg tracking-wide text-gray-12 opacity-54">{title}</h1>
-  <div class="flex gap-10 overflow-x-scroll px-10 opacity-50">
+  <div class="flex items-center justify-center gap-10 overflow-x-scroll px-10 opacity-50">
     {#each customers as { name, logo }}
       <img alt={name} src={logo} class="max-h-8" />
     {/each}
