a50d1fe2b120e69ae52ac36710daa17aa7c0107a
Pedro Brandão <pedro@significa.co>
2023-06-09 20:08:35 +0100

    feat: add page name placeholder

--- src/routes/[...path]/+page.svelte
+++ src/routes/[...path]/+page.svelte
@@ -5,5 +5,9 @@
 </script>
 
 {#key data.page.id}
+  <!-- TODO: remove this after we have heroes -->
+  <div class="container mx-auto mt-16 px-container py-8">
+    <h1 class="text-3xl">{data.page.name}</h1>
+  </div>
   <DynamicBlock block={data.page.content} />
 {/key}
