b99eb4bfef463aba013f7942ca821028aa3dd9ce
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2023-06-09 18:13:58 +0100

    feat(components): create `Platform` section for `Homepage`

---
+++ src/components/homepage/platform/platform.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import Title from '$components/title.svelte';
+
+  import Blur from './blur.svelte';
+  import ProductDisplay from './product-display.svelte';
+</script>
+
+<section
+  class="relative mx-auto w-full max-w-[1440px] pb-12 max-[1650px]:overflow-hidden md:grid md:grid-cols-2 md:gap-8 md:pt-[35px]"
+>
+  <div class="absolute right-[-88px] top-[-182px] hidden blur-[400px] md:block">
+    <Blur />
+  </div>
+  <div>
+    <Title
+      label={{ color: 'orange', content: 'Our Platform' }}
+      title="Infrastructure & tools to power your in-app communication"
+    />
+    <div class="px-5 md:mt-6 md:pl-16">
+      <ProductDisplay
+        illustration="/assets/homepage/product-1.png"
+        title="Messaging & Chat"
+        description="Flexible, secure, and easy-to-manage messaging for your web and mobile apps."
+        link={{ href: '/', label: 'Learn more' }}
+      />
+    </div>
+  </div>
+  <div class="mt-10 px-5 md:mt-[146px] md:pr-16">
+    <ProductDisplay
+      illustration="/assets/homepage/product-2.png"
+      title="Voice & Video Calling"
+      description="Enterprise-grade security, robust infrastructure, and a familiar UI from the same vendor as
+    your chat solution."
+      link={{ href: '/', label: 'Learn more' }}
+    />
+  </div>
+</section>
---
+++ src/components/homepage/platform/product-display.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import GhostButton from '$components/buttons/ghost-button.svelte';
+
+  export let illustration = '';
+  export let title = '';
+  export let description = '';
+  export let link = { label: '', href: '' };
+</script>
+
+<div class="w-full max-w-[640px]">
+  <img class="mb-6 md:mb-12" src={illustration} alt={title} />
+  <div class="max-w-[416px] font-sans text-xl text-gray-12">
+    <p class="mb-1 font-semibold leading-tighter md:mb-2">{title}</p>
+    <p class="mb-3 font-normal leading-snug tracking-wide opacity-74">{description}</p>
+    <GhostButton as="a" href={link.href} variant="highlighted">{link.label}</GhostButton>
+  </div>
+</div>
