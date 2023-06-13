5667605bc212708b68b7e7ae27f26b362f207f50
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2023-06-13 17:34:51 +0100

    chore(PlatformSection): update links with `getAnchorFromCmsLink`

--- src/components/blocks/platform-section.svelte
+++ src/components/blocks/platform-section.svelte
@@ -6,7 +6,7 @@
 
   import { storyblokEditable } from '$lib/actions/storyblok-editable';
   import type { PlatformSectionStoryblok } from '$types/bloks';
-  import { sanitizeSlug } from '$lib/storyblok';
+  import { getAnchorFromCmsLink } from '$lib/storyblok';
 
   export let block: PlatformSectionStoryblok;
 </script>
@@ -24,29 +24,29 @@
           label={{ color: 'orange', content: block.title[0].label }}
           title={block.title[0].title}
         />
-        <div class="px-5 md:mt-6 md:pl-16">
+        {#if block.products[0]}
+          {@const link = getAnchorFromCmsLink(block.products[0].link)}
+          <div class="px-5 md:mt-6 md:pl-16">
+            <ProductDisplay
+              illustration={block.products[0].image.filename}
+              title={block.products[0].title}
+              description={block.products[0].description}
+              link={{ ...link, label: block.products[0].link[0].label ?? '' }}
+            />
+          </div>
+        {/if}
+      </div>
+      {#if block.products[0]}
+        {@const link = getAnchorFromCmsLink(block.products[1].link)}
+        <div class="mt-10 px-5 md:mt-[146px] md:pr-16">
           <ProductDisplay
-            illustration={block.products[0].image.filename}
-            title={block.products[0].title}
-            description={block.products[0].description}
-            link={{
-              href: sanitizeSlug(block.products[0].link[0].link.cached_url),
-              label: block.products[0].link[0].label ?? ''
-            }}
+            illustration={block.products[1].image.filename}
+            title={block.products[1].title}
+            description={block.products[1].description}
+            link={{ ...link, label: block.products[1].link[0].label ?? '' }}
           />
         </div>
-      </div>
-      <div class="mt-10 px-5 md:mt-[146px] md:pr-16">
-        <ProductDisplay
-          illustration={block.products[1].image.filename}
-          title={block.products[1].title}
-          description={block.products[1].description}
-          link={{
-            href: sanitizeSlug(block.products[1].link[0].link.cached_url),
-            label: block.products[1].link[0].label ?? ''
-          }}
-        />
-      </div>
+      {/if}
     </div>
   </section>
 {/if}
--- src/components/homepage/platform/product-display.svelte
+++ src/components/homepage/platform/product-display.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import GhostButton from '$components/buttons/ghost-button.svelte';
+  import type { MultilinkStoryblok } from '$types/bloks';
 
   export let illustration = '';
   export let title = '';
   export let description = '';
-  export let link = { label: '', href: '' };
+  export let link: MultilinkStoryblok;
 </script>
 
 <div class="w-full max-w-[640px]">
@@ -12,6 +13,8 @@
   <div class="max-w-[416px] font-sans text-xl text-gray-12">
     <p class="mb-1 font-semibold leading-tighter md:mb-2">{title}</p>
     <p class="mb-3 font-normal leading-snug tracking-wide opacity-74">{description}</p>
-    <GhostButton as="a" href={link.href} variant="highlighted">{link.label}</GhostButton>
+    <GhostButton as="a" href={link.href} target={link.target} rel={link.rel} variant="highlighted">
+      {link.label}
+    </GhostButton>
   </div>
 </div>
