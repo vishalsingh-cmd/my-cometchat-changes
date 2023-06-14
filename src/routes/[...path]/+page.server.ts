b025f0512bf969a9bac5cc8f840d7198156c1e94
João Gomes <joaogomes@significa.co>
2023-06-14 19:02:14 +0100

    chore(customer-section): add to relation bloks

--- src/routes/[...path]/+page.server.ts
+++ src/routes/[...path]/+page.server.ts
@@ -9,9 +9,12 @@ export const load = async ({ cookies, fetch, params }) => {
   const version: 'draft' | 'published' = cookies.get(PREVIEW_COOKIE_KEY) ? 'draft' : 'published';
   const storyblok = getStoryblok({ fetch });
 
+  const relations = ['technologies-section.technologies', 'customer-stories-section.testimonials'];
+
   try {
     const page = await storyblok.get(`cdn/stories/pages/${params.path}`, {
-      version
+      version,
+      resolve_relations: relations
     });
 
     return {
