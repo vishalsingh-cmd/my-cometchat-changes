561fa97f31d1199cf4193fe3edfd783e2ae6b049
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2023-06-14 17:50:41 +0100

    feat(components): create `Panel`

---
+++ src/components/panel.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { InfoItemType } from '$components/info-item.svelte';
+  import InfoItem from '$components/info-item.svelte';
+
+  export let item: InfoItemType;
+  export let infoItemClassName: string | undefined;
+</script>
+
+<div
+  class="border-b border-gray-12/[.08] px-5 py-10 md:border-b-0 md:border-r md:px-16 md:py-[72px]"
+>
+  <InfoItem {item} class={infoItemClassName} />
+</div>
