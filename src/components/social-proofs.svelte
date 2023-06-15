a90aa4a2293eb6ce45142ac90c1f8e09b93152ab
João Gomes <joaogomes@significa.co>
2023-06-15 15:40:13 +0100

    feat(social-proofs): improve code and scroll behaviour

--- src/components/social-proofs.svelte
+++ src/components/social-proofs.svelte
@@ -1,21 +1,39 @@
 <script lang="ts">
-  type Customer = {
-    name: string;
-    logo: string;
-  };
+  import { getImageAttributes } from '$lib/storyblok';
+  import type { CustomerStoryblok, SocialProofsStoryblok } from '$types/bloks';
+  import { storyblokEditable } from '$lib/actions/storyblok-editable';
+  import type { StoryblokStory } from 'storyblok-generate-ts';
 
-  export let title: string;
-  export let customers: Customer[];
+  export let block: SocialProofsStoryblok;
+  const customers = block.customers as StoryblokStory<CustomerStoryblok>[];
 </script>
 
-<section
-  data-theme="dark"
-  class="flex flex-col items-center justify-center gap-8 bg-gray-1 pb-20 pt-16"
->
-  <h1 class="text-lg tracking-wide text-gray-12 opacity-54">{title}</h1>
-  <div class="flex items-center justify-center gap-10 overflow-x-scroll px-10 opacity-50">
-    {#each customers as { name, logo }}
-      <img alt={name} src={logo} class="max-h-8" />
-    {/each}
-  </div>
-</section>
+{#if block}
+  <section
+    data-theme="dark"
+    use:storyblokEditable={block}
+    class="flex flex-col items-center justify-center gap-8 bg-gray-1 pb-20 pt-16"
+  >
+    <h1 class="text-lg tracking-wide text-gray-12 opacity-54">{block.title}</h1>
+    {#if customers}
+      <div class="no-scrollbar flex gap-10 self-center overflow-x-scroll px-8 opacity-50">
+        {#each customers as customer}
+          <img alt="" src={getImageAttributes(customer.content.logo).src} class="max-h-6" />
+        {/each}
+      </div>
+    {/if}
+  </section>
+{/if}
+
+<style>
+  /* Hide scrollbar for Chrome, Safari and Opera */
+  .no-scrollbar::-webkit-scrollbar {
+    display: none;
+  }
+
+  /* Hide scrollbar for IE, Edge and Firefox */
+  .no-scrollbar {
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+  }
+</style>
