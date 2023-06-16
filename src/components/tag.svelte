973828d2601f44d955b578232b47449d40030c34
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2023-06-16 17:09:40 +0100

    refactor(style): remove `borderRadius` token of `10px`

--- src/components/tag.svelte
+++ src/components/tag.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-  class="w-fit rounded-10 bg-brand-10/[.12] px-3 py-1.5 text-md font-semibold leading-snug tracking-wide text-brand-10"
+  class="w-fit rounded-[10px] bg-brand-10/[.12] px-3 py-1.5 text-md font-semibold leading-snug tracking-wide text-brand-10"
 >
   {label}
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -109,8 +109,7 @@ export default {
         focus: `0 0 16px 2px ${theme('colors.brand.7/80')}`
       }),
       borderRadius: {
-        '1/2': '50%',
-        10: '10px'
+        '1/2': '50%'
       },
       transitionTimingFunction: {
         motion: 'cubic-bezier(0.2, 1, 0.2, 1)',
