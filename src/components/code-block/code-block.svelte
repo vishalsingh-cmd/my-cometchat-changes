572e4af7c7e4d416de8d642e9260b0715cd5ae9f
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2023-06-20 16:58:47 +0100

    feat(CodeBlock): ability to scroll on desktop

--- src/components/code-block/code-block.svelte
+++ src/components/code-block/code-block.svelte
@@ -33,15 +33,105 @@
   afterUpdate(() => {
     Prism.highlightAllUnder(el);
   });
+
+  let container: HTMLElement | undefined = undefined;
+  let isThereLeftOverflow = false;
+  let isThereRightOverflow = false;
+  let scrollMovement = 0;
+
+  $: if (container) {
+    const widthOfAllButtons = container.scrollWidth;
+
+    if (widthOfAllButtons > container.clientWidth) {
+      if (container.scrollLeft > 0) {
+        isThereLeftOverflow = true;
+      } else {
+        isThereLeftOverflow = false;
+      }
+
+      if (container.scrollLeft < widthOfAllButtons - container.clientWidth) {
+        isThereRightOverflow = true;
+      } else {
+        isThereRightOverflow = false;
+      }
+    }
+  }
+
+  const scrollElements = (direction: 'left' | 'right') => {
+    if (container) {
+      const movement = 200;
+
+      container.scrollBy({
+        left: direction === 'left' ? -movement : movement,
+        behavior: 'smooth'
+      });
+
+      if (direction === 'left') {
+        scrollMovement -= movement;
+      } else {
+        scrollMovement += movement;
+      }
+
+      if (scrollMovement > 0) {
+        isThereLeftOverflow = true;
+      } else {
+        isThereLeftOverflow = false;
+      }
+
+      if (scrollMovement < container.scrollWidth - container.clientWidth) {
+        isThereRightOverflow = true;
+      } else {
+        isThereRightOverflow = false;
+      }
+    }
+  };
 </script>
 
 <!-- eslint-disable svelte/no-at-html-tags -->
 
 <div
   data-theme="dark"
-  class={cn('rounded-3xl border border-solid border-gray-5 bg-gray-1 lg:bg-gray-2/60', className)}
+  class={cn(
+    'relative rounded-3xl border border-solid border-gray-5 bg-gray-1 lg:bg-gray-2/60',
+    className
+  )}
 >
-  <div class="flex overflow-x-auto shadow-[inset_0_-1px_0_0] shadow-gray-5">
+  {#if isThereLeftOverflow}
+    <div
+      class="absolute left-0 top-0 isolate z-20 flex h-[50px] w-[80px] items-center justify-start rounded-tl-3xl bg-gradient-to-r from-gray-2 from-50% to-gray-2/0"
+    >
+      <button
+        on:click={() => {
+          if (container) {
+            scrollElements('left');
+          }
+        }}
+        class="flex h-[50px] w-[50px] items-center justify-center"
+      >
+        <Icon icon="chevron-left-double" size="sm" class="block" />
+      </button>
+    </div>
+  {/if}
+  {#if isThereRightOverflow}
+    <div
+      class="absolute right-0 top-0 isolate z-20 flex h-[50px] w-[80px] items-center justify-end rounded-tr-3xl bg-gradient-to-l from-gray-2 from-50% to-gray-2/0"
+    >
+      <button
+        on:click={() => {
+          if (container) {
+            scrollElements('right');
+          }
+        }}
+        class="flex h-[50px] w-[50px] items-center justify-center"
+      >
+        <Icon icon="chevron-right-double" size="sm" class="block" />
+      </button>
+    </div>
+  {/if}
+  <div
+    bind:this={container}
+    class="flex overflow-x-auto shadow-[inset_0_-1px_0_0] shadow-gray-5 md:overflow-hidden"
+  >
     {#each snippets as { label }, i}
       <button
         on:click={() => {
