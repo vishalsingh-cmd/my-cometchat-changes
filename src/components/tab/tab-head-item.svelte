46005954768ab60f783d833f271b0d165a9ded10
João Gomes <joaogomes@significa.co>
2023-06-28 16:43:39 +0100

    refactor(tabs): blur item animation

--- src/components/tab/tab-head-item.svelte
+++ src/components/tab/tab-head-item.svelte
@@ -8,7 +8,7 @@
 
 <li
   class={cn(
-    'relative isolate w-fit overflow-hidden transition-all',
+    'relative isolate w-fit overflow-hidden',
     isActive ? 'border-b-[1px] border-b-brand-8' : ''
   )}
   role="presentation"
@@ -17,7 +17,7 @@
     on:click
     data-theme="light"
     class={cn(
-      'z-10 w-fit cursor-pointer p-5 text-xl/tighter font-semibold text-gray-12 opacity-54 transition-all hover:opacity-100 md:px-5 md:py-6',
+      'z-10 w-full cursor-pointer p-5 text-xl/tighter font-semibold text-gray-12 opacity-54 hover:opacity-100 md:px-5 md:py-6',
       isActive ? 'opacity-100' : ''
     )}
     id="{id}-tabhead"
@@ -28,28 +28,30 @@
   </button>
 
   {#if isActive}
-    <div
-      in:fade
-      out:fade
-      class="animate absolute -bottom-5 z-0 h-14 w-full"
-      style={cn(
-        'background: radial-gradient(50% 50.00% at 50% 50.00%, rgba(104, 82, 214) 0%, rgba(104, 82, 214, 0.00) 100%);'
-      )}
-    />
+    <div class="w-full">
+      <div
+        in:fade
+        out:fade
+        class="animate absolute -bottom-5 z-0 h-14 w-3/4"
+        style={cn(
+          'background: radial-gradient(50% 50.00% at 50% 50.00%, rgba(104, 82, 214) 0%, rgba(104, 82, 214, 0.00) 100%);'
+        )}
+      />
+    </div>
   {/if}
 </li>
 
 <style>
   .animate {
-    animation: 4s ease-in 0s infinite alternate both running slideBackAndFoward;
-    transform: translateX(10%) scale(100%);
-    opacity: 25%;
+    animation: 4s ease-in-out 0s infinite alternate both running slideBackAndFoward;
+    transform: translateX(0);
+    opacity: 12%;
   }
 
   @keyframes slideBackAndFoward {
     0% {
-      transform: translateX(10%) scale(100%);
-      opacity: 25%;
+      transform: translateX(0);
+      opacity: 12%;
     }
 
     25% {
@@ -57,16 +59,16 @@
     }
 
     50% {
-      opacity: 22%;
+      opacity: 12%;
     }
 
     75% {
-      opacity: 20%;
+      opacity: 18%;
     }
 
     100% {
-      transform: translateX(-10%);
-      opacity: 25%;
+      transform: translateX(34%);
+      opacity: 22%;
     }
   }
 </style>
