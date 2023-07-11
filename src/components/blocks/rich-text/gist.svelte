da8576a5d4c781a727340728a0fe75f21054af40
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2023-07-11 17:53:56 +0100

    feat(Gist): add styling

--- src/components/blocks/rich-text/gist.svelte
+++ src/components/blocks/rich-text/gist.svelte
@@ -15,6 +15,36 @@
       frame.style.height = `${frame.contentWindow?.document.body.scrollHeight + 24}px`;
 
       frame.contentWindow.document.body.style.margin = '0';
+
+      frame.contentWindow.document.head.innerHTML += `
+        <style>
+          .gist .gist-data {
+            background-color: hsla(246, 21%, 9%, 0.02) !important;
+          }
+
+          .gist .gist-file {
+            border: 1px solid hsla(246, 21%, 9%, 0.04) !important;
+            border-radius: 16px;
+          }
+
+          .gist .blob-code-content {
+            padding: 20px 0px !important;
+          }
+
+          .gist .highlight {
+            background: transparent !important;
+          }
+
+          .gist-meta {
+            border-radius: 0px 0px 16px 16px !important;
+            background-color: hsla(246, 21%, 9%, 0.02) !important;
+          }
+
+          .gist-meta a {
+            color: #6852D6 !important;
+          }
+        </style>
+      `;
     });
   });
 </script>
@@ -24,6 +54,3 @@
     <iframe class="container w-full" src="about:blank" bind:this={frame} title="Gist" />
   </div>
 {/if}
-
-<style lang="postcss">
-</style>
