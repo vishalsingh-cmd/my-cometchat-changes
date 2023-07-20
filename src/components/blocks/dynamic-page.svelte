569e961d8fc5270fc4b1ad0ad6b8d0630bd06f67
João Gomes <joaogomes@significa.co>
2023-07-20 13:42:05 +0100

    feat(tutorial): add tutorial to dynamic-page

--- src/components/blocks/dynamic-page.svelte
+++ src/components/blocks/dynamic-page.svelte
@@ -3,28 +3,37 @@
     BlogPostStoryblok,
     CustomerStoryStoryblok,
     PageStoryblok,
-    TechnologyStoryblok
+    TechnologyStoryblok,
+    TutorialStoryblok
   } from '$types/bloks';
   import type { ISbStoryData } from '@storyblok/js';
 
+  import Page from './page.svelte';
+  import Tutorial from './tutorial.svelte';
   import BlogPost from './blog-post.svelte';
   import CustomerStory from './customer-story.svelte';
-  import Page from './page.svelte';
 
   /**
    * The dynamic page will render the correct page based on the content type (page, blog-post, etc.)
    */
   export let page: ISbStoryData<
-    PageStoryblok | CustomerStoryStoryblok | TechnologyStoryblok | BlogPostStoryblok
+    | PageStoryblok
+    | CustomerStoryStoryblok
+    | TechnologyStoryblok
+    | BlogPostStoryblok
+    | TutorialStoryblok
   >;
 
   const pageData = page as unknown as PageStoryblok;
   const blogPostData = page as unknown as BlogPostStoryblok;
+  const tutorialData = page as unknown as TutorialStoryblok;
   const customerStoryData = page as unknown as CustomerStoryStoryblok;
 </script>
 
 {#if page.content.component === 'blog-post'}
   <BlogPost block={blogPostData} />
+{:else if page.content.component === 'tutorial'}
+  <Tutorial block={tutorialData} />
 {:else if page.content.component === 'customer-story'}
   <CustomerStory block={customerStoryData} />
 {:else}
