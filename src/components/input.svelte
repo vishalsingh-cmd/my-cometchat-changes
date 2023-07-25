4253346ac42a3361145838cceb0173d59b9ad344
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2023-07-25 17:07:13 +0100

    feat(components): create `Input`

---
+++ src/components/input.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import type { HTMLInputAttributes } from 'svelte/elements';
+
+  import { cn } from '$lib/utils';
+
+  import Icon from '$components/icon/icon.svelte';
+
+  interface $$Props extends HTMLInputAttributes {
+    icon?: string;
+  }
+
+  export let value: HTMLInputAttributes['value'] = undefined;
+  export let icon: $$Props['icon'] = undefined;
+</script>
+
+<label for={$$props.id} class="relative text-gray-12">
+  {#if icon}
+    <Icon {icon} size="xs" class="absolute left-4 top-1/2 -translate-y-2/4 text-gray-12/74" />
+  {/if}
+  <input
+    class={cn(
+      'w-[304px]',
+      'rounded-[14px] border border-gray-12/10 bg-gray-12/[0.02]',
+      'px-4 py-[13px]',
+      'text-md font-semibold leading-tight tracking-wide',
+      'placeholder:text-opacity-64',
+      'hover:border-brand-9/30',
+      'focus-visible:border-brand-9/40 focus-visible:shadow-focus focus-visible:outline-none focus-visible:ring-4',
+      icon && 'pl-10',
+      value && value.length > 0 && 'pr-10'
+    )}
+    bind:value
+    on:change
+    on:mouseenter
+    on:mouseleave
+    on:click
+    on:focus
+    {...$$restProps}
+  />
+  {#if value && value.length > 0}
+    <button
+      class="absolute right-4 top-1/2 h-[14px] w-[14px] -translate-y-2/4 text-gray-12/74"
+      on:click={() => (value = '')}
+    >
+      <Icon size="xs" icon="x-circle" />
+    </button>
+  {/if}
+</label>
