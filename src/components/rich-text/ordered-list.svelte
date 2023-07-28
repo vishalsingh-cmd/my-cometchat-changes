3b7701f4f5a84fc7aca8be850b3d9f1bc6705a62
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2023-07-28 12:53:59 +0100

    feat(RichTextRenderer): create an `ordere_list` styling

---
+++ src/components/rich-text/ordered-list.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { RichTextResolver, type ISbRichtext } from '@storyblok/js';
+
+  export let content: ISbRichtext;
+
+  const resolver = new RichTextResolver();
+</script>
+
+{#if content.content}
+  <ol class="pl-4">
+    {#each content.content as item}
+      <li class="mb-3 text-lg-richtext font-medium leading-snug tracking-wide">
+        <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+        {@html resolver.render(item)}
+      </li>
+    {/each}
+  </ol>
+{/if}
