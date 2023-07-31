19f1d7491b82efd17cf2e5edc3950bb6d9ee250f
João Gomes <joaogomes@significa.co>
2023-07-31 15:16:54 +0100

    fix(lists-section): replace tag to badge & fix badge & fix coming soon item

--- src/components/lists-section/item.svelte
+++ src/components/lists-section/item.svelte
@@ -4,19 +4,19 @@
   import type { BulletPointStoryblok } from '$types/bloks';
 
   import Icon from '$components/icon/icon.svelte';
-  import Tag from '$components/tag.svelte';
+  import Badge from '$components/badge.svelte';
 
   export let item: BulletPointStoryblok;
 </script>
 
 <div class="flex gap-2">
   <Icon icon="star-04" size="xs" class="flex-shrink-0 pt-px text-brand-9" />
-  <div class="flex flex-wrap">
+  <div class="flex flex-wrap gap-y-2">
     <p class="text-lg font-medium leading-snug tracking-wide opacity-74">
       {item.item}
     </p>
     {#if item.coming_soon}
-      <Tag label={string('coming_soon')} />
+      <Badge label={string('coming_soon')} size="medium" />
     {/if}
   </div>
 </div>
