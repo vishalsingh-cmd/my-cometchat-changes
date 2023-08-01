df6b1484f7c54c999fc31207a2b6805c810ca3fd
João Gomes <joaogomes@significa.co>
2023-08-01 12:23:38 +0100

    feat(chat-and-messaging-hero): improve asset gradient

--- src/components/chat-and-messaging/hero/asset-gradients.svelte
+++ src/components/chat-and-messaging/hero/asset-gradients.svelte
@@ -7,7 +7,7 @@
 <div
   class={cn(
     ellipse(),
-    '-bottom-[20px] -left-[30px] h-[250px] w-[100px] bg-gray-1 blur-[20px] md:-left-[350px] md:h-[600px] md:w-[523px] md:blur-[58px] 2xl:left-[200px]'
+    '-bottom-[20px] -left-[30px] h-[250px] w-[100px] bg-gray-1 blur-[20px] md:-left-[350px] md:h-[600px] md:w-[523px] md:blur-[58px] 2xl:w-[1000px]'
   )}
   style="transform: translate3d(0, 0, 0)"
 />
