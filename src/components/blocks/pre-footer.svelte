91c479d492cbd1c487e6e163cd88a7d2fd7205e7
João Gomes <joaogomes@significa.co>
2023-08-01 14:49:56 +0100

    fix(pre-footer): contain content to match with other sections

--- src/components/blocks/pre-footer.svelte
+++ src/components/blocks/pre-footer.svelte
@@ -13,42 +13,39 @@
 </script>
 
 {#if block}
-  <section
-    data-theme="dark"
-    class="relative overflow-hidden bg-gray-1"
-    use:storyblokEditable={block}
-  >
-    <div
-      class="flex flex-col items-start justify-center px-5 pb-20 pt-24 sm:items-center xl:px-0 xl:py-36"
-    >
-      <div class="flex flex-col items-start sm:items-center">
-        <p class="z-20 text-3xl text-gray-12">{block.title}</p>
-        <p
-          class="z-20 mt-1 max-w-md whitespace-pre-line text-xl leading-tight text-gray-12 opacity-70 sm:text-center"
-        >
-          {block.description}
-        </p>
-      </div>
-      {#if block.call_to_action}
-        <div class="z-20 mt-8 flex gap-3 text-gray-12">
-          {#each block.call_to_action as button}
-            {@const { href, target, rel } = getAnchorFromCmsLink(button.link)}
-            <Button as="a" {href} {target} {rel} variant={button.variant}>{button.label}</Button>
-          {/each}
+  <section data-theme="dark" class="overflow-hidden" use:storyblokEditable={block}>
+    <div class="container relative mx-auto bg-gray-1 px-container">
+      <div class="flex flex-col items-start justify-center pb-20 pt-24 sm:items-center xl:py-36">
+        <div class="flex flex-col items-start sm:items-center">
+          <p class="z-20 text-3xl text-gray-12">{block.title}</p>
+          <p
+            class="z-20 mt-1 max-w-md whitespace-pre-line text-xl leading-tight text-gray-12 opacity-70 sm:text-center"
+          >
+            {block.description}
+          </p>
         </div>
-      {/if}
+        {#if block.call_to_action}
+          <div class="z-20 mt-8 flex gap-3 text-gray-12">
+            {#each block.call_to_action as button}
+              {@const { href, target, rel } = getAnchorFromCmsLink(button.link)}
+              <Button as="a" {href} {target} {rel} variant={button.variant}>{button.label}</Button>
+            {/each}
+          </div>
+        {/if}
+      </div>
+
+      <img alt="" src={Stars} class="absolute inset-2 z-10" />
+      <img alt="" src={Ellipse} class="absolute top-0 w-full" />
+      <img
+        alt=""
+        src={BigComet}
+        class="absolute -left-[100px] -top-[120px] h-[250px] w-[250px] lg:-left-[200px] lg:-top-[200px] lg:h-[550px] lg:w-[550px]"
+      />
+      <img
+        alt=""
+        src={SmallComet}
+        class="absolute -bottom-36 -right-[200px] z-10 h-[420px] w-[420px] lg:-bottom-32 lg:-right-[100px] lg:h-[500px] lg:w-[500px]"
+      />
     </div>
-    <img alt="" src={Stars} class="absolute inset-2 z-10" />
-    <img alt="" src={Ellipse} class="absolute top-0 hidden w-full lg:block" />
-    <img
-      alt=""
-      src={BigComet}
-      class="absolute -left-[100px] -top-[120px] z-10 h-[250px] w-[250px] lg:-left-[200px] lg:-top-[200px] lg:h-[550px] lg:w-[550px]"
-    />
-    <img
-      alt=""
-      src={SmallComet}
-      class="absolute -bottom-36 -right-[200px] z-10 h-[420px] w-[420px] lg:-bottom-32 lg:-right-[100px] lg:h-[500px] lg:w-[500px]"
-    />
   </section>
 {/if}
