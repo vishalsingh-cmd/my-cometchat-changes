7d8c93634e92703346f287a1be82f866cc2b8cfb
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2023-08-03 11:27:16 +0100

    fix(Footer): number of `columns` on screens

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -63,9 +63,7 @@
     </div>
 
     <!-- main links - Desktop -->
-    <div
-      class="hidden pb-10 sm:grid sm:grid-cols-2 sm:gap-20 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5"
-    >
+    <div class="hidden pb-10 sm:grid sm:grid-cols-2 sm:gap-20 lg:grid-cols-3 xl:grid-cols-5">
       {#each data.columns as columnGroup}
         <div class="flex flex-col sm:gap-20">
           {#each columnGroup as column}
