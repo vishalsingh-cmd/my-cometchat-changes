e23e1219178e2a6f87952085cbf1fecf709cd1d2
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2023-08-03 12:47:24 +0100

    fix(CodeBlock): remove reference to `height`

--- src/components/code-block/code-block.svelte
+++ src/components/code-block/code-block.svelte
@@ -97,7 +97,7 @@
 <div
   data-theme="dark"
   class={cn(
-    'relative flex h-full max-h-[480px] flex-col rounded-3xl border border-solid border-gray-5 bg-gray-1 lg:bg-gray-2/60',
+    'relative flex h-full flex-col rounded-3xl border border-solid border-gray-5 bg-gray-1 lg:bg-gray-2/60',
     className
   )}
 >
@@ -167,8 +167,7 @@
         data-line={lineHighlight}
         class={clsx('h-full py-4 md:py-6', { 'line-numbers': lineNumbers }, className)}
         data-start={lineNumbersStartAt}
-        {...$$restProps}>
-  <code class="language-{language}"
+        {...$$restProps}><code class="language-{language}"
           >{@html Prism.highlight(code, Prism.languages[language], language)}</code
         >
 </pre>
