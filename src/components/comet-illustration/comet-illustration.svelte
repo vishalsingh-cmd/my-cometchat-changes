90cf4990b83f4fa8ebda1d6fa976d3baec0bccbd
João Gomes <joaogomes@significa.co>
2023-08-07 14:34:02 +0100

    refactor(comet-illustrations): update comet illustrations

--- src/components/comet-illustration/comet-illustration.svelte
+++ src/components/comet-illustration/comet-illustration.svelte
@@ -35,7 +35,7 @@
   import { cva, type VariantProps } from 'class-variance-authority';
   import { cn } from '$lib/utils';
 
-  export const illustrationStyle = cva(['inline-block scale-[1.4]'], {
+  export const illustrationStyle = cva(['inline-block scale-[2]'], {
     variants: {
       size: {
         sm: ['h-5', 'w-5'],
