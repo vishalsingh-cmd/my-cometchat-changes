60c12bc761d1a7ecaa650b6f2ff00fcba0479586
João Gomes <joaogomes@significa.co>
2023-08-09 12:10:04 +0100

    fix(badge): fix padding and line-height

--- src/components/badge.svelte
+++ src/components/badge.svelte
@@ -7,15 +7,15 @@
 
   const badge = cva(
     [
-      'inline-flex items-center whitespace-nowrap',
-      'font-semibold tracking-wide leading-none text-brand-10',
+      'inline-flex items-center flex-wrap',
+      'font-semibold tracking-wide text-brand-10',
       'border-[1.4px] light:border-brand-10/[0.16] dark:border-brand-10/[0.16]'
     ],
     {
       variants: {
         size: {
-          small: ['h-[22px]', 'px-1.5', 'text-xs', 'rounded-[7px]'],
-          medium: ['h-[32px]', 'px-2.5', 'text-md', 'rounded-[10px]']
+          small: ['py-0.5', 'px-1.5', 'text-xs', 'rounded-[7px]', 'leading-normal'],
+          medium: ['py-[5.5px]', 'px-2.5', 'text-md', 'rounded-[10px]', 'leading-snug']
         }
       }
     }
