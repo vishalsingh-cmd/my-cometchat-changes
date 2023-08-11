b0769e1965c372a676397f41cedb5b6bf0cf79f2
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2023-08-11 19:18:33 +0100

    chore(HomepageHero): remove bottom padding do decrease distance to logos

--- src/components/blocks/homepage-hero.svelte
+++ src/components/blocks/homepage-hero.svelte
@@ -19,7 +19,7 @@
   <section
     data-theme="dark"
     use:storyblokEditable={block}
-    class="relative overflow-hidden bg-gray-1 px-5 pb-20 pt-24 md:px-20 md:pt-[188px]"
+    class="relative overflow-hidden bg-gray-1 px-5 pt-24 md:px-20 md:pt-[188px]"
   >
     <div class="absolute left-[calc(50vw-700px)] top-[80px] hidden md:block">
       <img src={OrbitMedium} alt="" />
