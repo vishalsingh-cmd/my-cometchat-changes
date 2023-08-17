4ffb564a4703cc4fddc05015a2b27a63282f86f9
João Gomes <joaogomes@significa.co>
2023-08-17 14:39:27 +0100

    fix(disclaimer-card): padding

--- src/components/blocks/rich-text/disclaimer.svelte
+++ src/components/blocks/rich-text/disclaimer.svelte
@@ -9,7 +9,7 @@
 {#if block}
   <div
     use:storyblokEditable={block}
-    class="my-5 flex flex-col gap-1 rounded-2xl border border-gray-12/[0.04] bg-gray-12/[0.02] p-5 text-md font-medium leading-tight tracking-wide backdrop-blur-[20px] md:my-8 md:gap-2 md:p-4"
+    class="my-5 flex flex-col gap-1 rounded-2xl border border-gray-12/[0.04] bg-gray-12/[0.02] p-4 text-md font-medium leading-tight tracking-wide backdrop-blur-[20px] md:my-8 md:gap-2 md:p-5"
     style="transform: translate3d(0, 0, 0);"
   >
     <p class="opacity-74">{block.label}</p>
