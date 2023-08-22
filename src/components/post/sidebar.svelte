6463fe047f4fdd56b71ec8cb94871c8b07358d4a
Filipe Ferreira <filipeferreira@significa.co>
2023-08-22 16:17:34 +0100

    feat(post): add share at the when on mobile COM-454

--- src/components/post/sidebar.svelte
+++ src/components/post/sidebar.svelte
@@ -1,37 +1,10 @@
 <script lang="ts">
   import { createEventDispatcher } from 'svelte';
 
-  import { page } from '$app/stores';
-
-  import { string } from '$lib/strings';
   import { cn } from '$lib/utils';
 
-  import Icon from '$components/icon/icon.svelte';
   import Background from './assets/background.png';
-
-  const createAbsoluteUrl = (url: string) => {
-    return encodeURIComponent(url);
-  };
-
-  const shareLinks = [
-    {
-      icon: 'facebook',
-      text: string('blog.facebook'),
-      url: `https://www.facebook.com/sharer/sharer.php?u=${createAbsoluteUrl($page.url.href)}`
-    },
-    {
-      icon: 'twitter',
-      text: string('blog.twitter'),
-      url: `https://twitter.com/intent/tweet?url=${createAbsoluteUrl($page.url.href)}`
-    },
-    {
-      icon: 'linkedin',
-      text: string('blog.linkedin'),
-      url: `https://www.linkedin.com/shareArticle?mini=true&url=${createAbsoluteUrl(
-        $page.url.href
-      )}`
-    }
-  ];
+  import Share from './share.svelte';
 
   const dispatch = createEventDispatcher();
 
@@ -39,14 +12,16 @@
   export let activeHeadingIndex = 0;
 </script>
 
-<div class="wrap sticky top-[80px] hidden h-fit w-full max-w-[272px] overflow-visible md:block">
+<div
+  class="wrap sticky top-[80px] hidden h-fit w-full max-w-[272px] overflow-visible pb-6 md:block"
+>
   <div class="relative flex h-full w-fit flex-col justify-between">
     <div class="absolute bottom-0 left-0 top-0 w-px bg-gradient-to-b from-gray-5 to-gray-5/0" />
     <img
       src={Background}
       alt=""
       draggable="false"
-      class="pointer-events-none absolute -bottom-1/3 -right-1/4 min-h-[680px] min-w-[680px] select-none opacity-20"
+      class="pointer-events-none absolute -bottom-1/2 -right-1/4 min-h-[680px] min-w-[680px] select-none opacity-20"
     />
     <div class="ml-3 inline-flex flex-col items-start">
       {#if headings.length > 0}
@@ -74,23 +49,6 @@
         {/each}
       {/if}
     </div>
-    <div class="ml-3 mt-[100px] flex flex-col gap-6 text-lg font-semibold leading-tight">
-      <p>
-        {string('blog.share')}
-      </p>
-      <div class="flex flex-col gap-3">
-        {#each shareLinks as link}
-          <a
-            href={link.url}
-            class="flex items-center gap-1.5 opacity-74 ease-smooth hover:opacity-100"
-            target="_blank"
-            rel="noopener"
-          >
-            <Icon icon={link.icon} size="xs" />
-            {link.text}
-          </a>
-        {/each}
-      </div>
-    </div>
+    <Share class="ml-3 mt-[100px]" />
   </div>
 </div>
