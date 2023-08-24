cf53e0cfcbdc882dd5b01a089a08e45c745d9113
João Gomes <joaogomes@significa.co>
2023-08-24 15:43:01 +0100

    refactor(standard-hero): bottom gradient conditions

--- src/components/blocks/standard-hero.svelte
+++ src/components/blocks/standard-hero.svelte
@@ -96,7 +96,7 @@
       <div
         class="absolute bottom-0 left-0 isolate z-20 h-[213px] w-full bg-gradient-to-t from-gray-1/100 to-gray-1/0"
       />
-    {:else}
+    {:else if !(block.image && block.image.filename !== '' && block.image.filename !== null && block.header_alignment === 'left')}
       <div
         class="absolute bottom-0 h-[187px] w-full bg-gradient-to-t from-gray-1/100 to-gray-1/0"
       />
