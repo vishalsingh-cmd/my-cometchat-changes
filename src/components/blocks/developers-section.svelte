06a966f874eb6da9acfe72e3c8b6007a9df5c0d6
João Gomes <joaogomes@significa.co>
2023-08-25 15:13:44 +0100

    fix(developers-section): add bottom gradient to prevent the illustration from being cropped

--- src/components/blocks/developers-section.svelte
+++ src/components/blocks/developers-section.svelte
@@ -36,7 +36,7 @@
 </script>
 
 {#if block}
-  <section use:storyblokEditable={block} class="overflow-x-hidden">
+  <section use:storyblokEditable={block} class="relative overflow-hidden">
     <div class="relative mx-auto max-w-content">
       <img
         src={Comet}
@@ -94,5 +94,10 @@
         </div>
       {/if}
     </div>
+
+    <!-- Bottom Gradient -->
+    <div
+      class="absolute bottom-0 left-0 h-[50px] w-screen bg-gradient-to-t from-gray-1/100 to-gray-1/0 lg:h-[100px]"
+    />
   </section>
 {/if}
