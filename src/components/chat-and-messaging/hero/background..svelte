530e88763affe031fb329496b5e829f070e869d2
João Gomes <joaogomes@significa.co>
2023-08-25 14:05:57 +0100

    fix(chat-messaging-hero): enhance gradients

---
+++ src/components/chat-and-messaging/hero/background..svelte
@@ -0,0 +1,6 @@
+<div
+  class="absolute bottom-0 left-0 h-[100px] w-screen bg-gradient-to-t from-gray-1/100 to-gray-1/0 lg:h-[300px]"
+/>
+<div
+  class="absolute -left-[100px] bottom-0 hidden h-[500px] w-[300px] bg-gradient-to-r from-gray-1/100 via-gray-1/100 to-gray-1/0 blur-xl md:block md:w-[30vw] 2xl:w-[50vw]"
+/>
