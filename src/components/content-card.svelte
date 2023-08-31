76a69cad67ecfb8c3b315b11f253371fb83da6c8
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2023-08-31 12:36:47 +0100

    chore(ContentCard): use `placeholder-image` if there's no image selected on Storyblok

--- src/components/content-card.svelte
+++ src/components/content-card.svelte
@@ -23,20 +23,26 @@
     href={sanitizeSlug(link)}
     class="group relative flex flex-col font-semibold text-gray-12 outline-none focus-visible:after:absolute focus-visible:after:-left-2 focus-visible:after:-top-2 focus-visible:after:h-[calc(100%+16px)] focus-visible:after:w-[calc(100%+16px)] focus-visible:after:rounded-[18px] focus-visible:after:border focus-visible:after:border-brand-7"
   >
-    {#if image}
-      {#if customer}
-        <div class="absolute left-3 top-4 rounded-xl bg-gray-12/20 p-2 backdrop-blur-[50px]">
-          <Media
-            imageTransformOptions={{ size: [0, 200] }}
-            media={customer.logo}
-            class="h-3.5 w-full"
-          />
-        </div>
-      {/if}
+    {#if customer}
+      <div class="absolute left-3 top-4 rounded-xl bg-gray-12/20 p-2 backdrop-blur-[50px]">
+        <Media
+          imageTransformOptions={{ size: [0, 200] }}
+          media={customer.logo}
+          class="h-3.5 w-full"
+        />
+      </div>
+    {/if}
+    {#if image && image.filename}
       <Media
         imageTransformOptions={{ size: [0, 800] }}
         media={image}
-        class="mb-4 aspect-video h-full max-h-[202px] w-full rounded-2xl object-cover md:mb-5 md:max-h-[280px]"
+        class="mb-4 h-[202px] w-full rounded-2xl object-cover md:mb-5 md:h-[240px]"
+      />
+    {:else}
+      <img
+        alt=""
+        src="./assets/placeholder-image.jpg"
+        class="mb-4 h-[202px] w-full rounded-2xl object-cover md:mb-5 md:h-[240px]"
       />
     {/if}
     {#if title}
