ae1dc60fd1c1d6a950c10b3a7b863f3d3ba22cd4
Joao Gomes <joaogomes@significa.co>
2023-09-01 19:11:15 +0100

    feat(features-at-glance): add dropdown on mobile instead of buttonTabs & create parent element on the sticky tabs so that we can translate them

--- src/components/blocks/features-at-glance-section.svelte
+++ src/components/blocks/features-at-glance-section.svelte
@@ -1,24 +1,35 @@
 <script lang="ts">
-  import ButtonTab from '$components/features-at-glance-section/button-tab.svelte';
-  import ListSectionItem from '$components/list-section/list-section-item.svelte';
+  import { storyblokEditable } from '$lib/actions/storyblok-editable';
+  import type { FeaturesAtGlanceSectionStoryblok } from '$types/bloks';
+
+  import { cn } from '$lib/utils';
+  import scrollDirection from '$lib/stores/scroll-direction';
+
   import Media from '$components/media.svelte';
   import Tabs from '$components/tabs/tabs.svelte';
-  import { intersectionObserver } from '$lib/actions/intersection-observer';
-  import { storyblokEditable } from '$lib/actions/storyblok-editable';
-  import scrollDirection from '$lib/stores/scroll-direction';
-  import { cn } from '$lib/utils';
-  import type { FeaturesAtGlanceSectionStoryblok } from '$types/bloks';
   import ListSection from './list-section.svelte';
+  import Dropdown from '$components/dropdown.svelte';
+  import ButtonTab from '$components/features-at-glance-section/button-tab.svelte';
+  import ListSectionItem from '$components/list-section/list-section-item.svelte';
 
   let activeTab = 0;
   let activeButtonsTab = 0;
+  let selectedFeatureIndex = 0;
 
-  let isIntersecting = false;
+  let windowScroll = 0;
+  let isSticky = false;
+  let topOffset: number;
+  let containerRef: HTMLElement;
+
+  $: if (windowScroll !== 0) {
+    topOffset = containerRef.getBoundingClientRect().top; // Distance from the element to the top
+    isSticky = topOffset === 0;
+  }
 
   export let block: FeaturesAtGlanceSectionStoryblok;
 </script>
 
-<!-- TODO: colocar borda quando estiver sticky -->
+<svelte:window bind:scrollY={windowScroll} />
 
 {#if block}
   <section use:storyblokEditable={block} data-theme="light" class="relative bg-gray-1 text-gray-12">
@@ -30,62 +41,77 @@
       </p>
     {/if}
     {#if block.features}
-      {@const parsedFeatures = block.features.map((item, i) => ({
+      {@const parsedTabsFeatures = block.features.map(({ title }, i) => ({
         id: i,
-        label: item.title
+        label: title
       }))}
-      <div
-        use:intersectionObserver={{
-          callback: ([e]) => {
-            if (e.isIntersecting) {
-              isIntersecting = true;
-            }
-          },
-          options: {}
-        }}
-        class={cn(
-          'sticky left-0 top-0 z-10 bg-gray-1 pb-8 transition-transform duration-300 ease-motion md:static md:pb-6',
-          {
-            animate: $scrollDirection === 'up'
-          }
-        )}
-      >
-        <Tabs
-          {activeTab}
-          options={parsedFeatures}
-          on:optionSelect={(e) => {
-            activeTab = e.detail.i;
-            activeButtonsTab = 0;
-          }}
-          class="container mx-auto px-container"
-        />
+      {@const parsedDropdownFeatures = block.features[activeTab].feature_list.map(
+        ({ title }, i) => ({
+          label: title,
+          value: title
+        })
+      )}
 
-        <div class="border-t border-gray-12/8">
-          <div
-            class="container mx-auto mt-4 flex w-full flex-row gap-3 overflow-x-scroll break-all px-container"
-          >
-            {#each block.features[activeTab].feature_list as feature, i}
-              <ButtonTab
-                id={i}
-                label={feature.title}
-                isActive={activeButtonsTab === i}
-                on:click={() => (activeButtonsTab = i)}
+      <!-- Sticky Element -->
+      <div bind:this={containerRef} class="sticky left-0 top-0 z-10 md:static">
+        <!-- Element that is going to translate -->
+        <div
+          class={cn('bg-gray-1 transition-transform duration-300 ease-motion', {
+            'border-b border-gray-12/8': isSticky,
+            'translate-y-16': $scrollDirection === 'up' && isSticky
+          })}
+        >
+          <Tabs
+            {activeTab}
+            options={parsedTabsFeatures}
+            on:optionSelect={(e) => {
+              activeTab = e.detail.i;
+              activeButtonsTab = 0;
+              selectedFeatureIndex = 0;
+            }}
+            class="container mx-auto px-container"
+          />
+
+          <div class="border-t border-gray-12/8">
+            <!-- Mobile Dropdown -->
+            <div class="container mx-auto w-full px-container md:hidden">
+              <Dropdown
+                class="my-4 w-full justify-between"
+                flyOutClass="w-full justify-between"
+                options={parsedDropdownFeatures}
+                selectedOption={selectedFeatureIndex}
+                on:optionSelect={(e) => {
+                  selectedFeatureIndex = e.detail.i;
+                  activeButtonsTab = e.detail.i;
+                }}
               />
-            {/each}
+            </div>
+
+            <!-- Desktop Tabs -->
+            <div
+              class="container mx-auto mt-4 hidden w-full flex-row gap-3 overflow-x-scroll break-all px-container md:flex"
+            >
+              {#each block.features[activeTab].feature_list as feature, i}
+                <ButtonTab
+                  id={i}
+                  label={feature.title}
+                  isActive={activeButtonsTab === i}
+                  on:click={() => (activeButtonsTab = i)}
+                />
+              {/each}
+            </div>
           </div>
         </div>
       </div>
 
-      <div class="container mx-auto px-container">
+      <div class="container mx-auto px-container pb-8 md:pb-12 md:pt-12">
         {#if block.features[activeTab].feature_list[activeButtonsTab]}
           {@const { image, items_right_of_image } =
             block.features[activeTab].feature_list[activeButtonsTab]}
-          <div
-            class=" grid grid-cols-1 gap-12 pb-8 md:pb-12 lg:mt-12 lg:grid-cols-[2fr,1fr] lg:gap-[54px]"
-          >
+          <div class="grid grid-cols-1 gap-12 lg:grid-cols-[2fr,1fr] lg:gap-[54px]">
             {#if image}
               <div
-                class="order-2 flex max-w-[864px] items-center justify-center rounded-3xl border border-gray-12/[.04] bg-gray-12/[0.02] py-8 backdrop-blur-[20px] lg:order-1"
+                class="order-2 flex max-w-[864px] items-center justify-center overflow-hidden rounded-3xl border border-gray-12/[.04] bg-gray-12/[0.02] py-8 backdrop-blur-[20px] lg:order-1"
               >
                 <div class="min-w-[318px] max-w-[555px] lg:w-full">
                   <Media
@@ -99,7 +125,7 @@
 
             {#if items_right_of_image}
               <div
-                class="order-1 flex flex-col gap-12 md:grid md:grid-cols-2 md:gap-10 lg:order-2 lg:grid-cols-1 lg:flex-col xl:flex"
+                class="order-1 mt-8 flex flex-col gap-12 md:mt-0 md:grid md:grid-cols-2 md:gap-10 lg:order-2 lg:grid-cols-1 lg:flex-col xl:flex"
               >
                 {#each items_right_of_image as item}
                   <ListSectionItem accentColour="brand" block={item} />
@@ -119,9 +145,3 @@
     {/if}
   </section>
 {/if}
-
-<style>
-  .animate {
-    transition: top 300ms ease-in-out;
-  }
-</style>
