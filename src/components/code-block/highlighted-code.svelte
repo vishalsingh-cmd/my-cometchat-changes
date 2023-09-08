cade71b717b5bd1911a54ca866b19e69dda75fd5
Joao Gomes <joaogomes@significa.co>
2023-09-08 17:57:08 +0100

    refactor(code-block): change code syntax highlighting lib to svelte-highlight & define the styles & create logics to accept only the supported languages

---
+++ src/components/code-block/highlighted-code.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { Highlight, LineNumbers } from 'svelte-highlight';
+
+  export let language: string;
+  export let code: string;
+  import './code.css';
+
+  const languages = {
+    javascript: () => import('svelte-highlight/languages/javascript'),
+    typescript: () => import('svelte-highlight/languages/typescript'),
+    swift: () => import('svelte-highlight/languages/swift'),
+    java: () => import('svelte-highlight/languages/java'),
+    kotlin: () => import('svelte-highlight/languages/kotlin'),
+    php: () => import('svelte-highlight/languages/php'),
+    dart: () => import('svelte-highlight/languages/dart')
+  } as const;
+
+  function isValidLanguage(language: string): language is keyof typeof languages {
+    return Object.keys(languages).includes(language);
+  }
+</script>
+
+{#if isValidLanguage(language)}
+  {#await languages[language]() then { default: language }}
+    <Highlight {language} {code} let:highlighted>
+      <LineNumbers {highlighted} wrapLines />
+    </Highlight>
+  {/await}
+{/if}
