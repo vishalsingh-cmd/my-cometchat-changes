b2b65ddd25d71458e1ac4dc5eb9d780807357f40
Francisco Marques <franciscopcmarques@gmail.com>
2023-09-08 15:22:24 +0100

    fix: error log

--- src/routes/[...path]/+page.server.ts
+++ src/routes/[...path]/+page.server.ts
@@ -99,8 +99,8 @@ export const load = async ({ cookies, fetch, params }) => {
       datasourceIntegrationTools
     };
   } catch (err) {
-    console.error(err);
     if (isStatusError(err) && err.status === 404) throw error(404, 'Not found');
+    console.error('Unhandled error in page.server:', err);
     throw err;
   }
 };
