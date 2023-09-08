a7d3e47487bf1c1e3f4ec6cb232e00e0af68c7e2
Joao Gomes <joaogomes@significa.co>
2023-09-08 12:44:48 +0100

    test: comment code

--- src/components/code-block/code-block.svelte
+++ src/components/code-block/code-block.svelte
@@ -10,7 +10,7 @@
   // import 'prismjs/components/prism-bash';
   // import 'prismjs/components/prism-elixir';
   // import 'prismjs/components/prism-go';
-  import 'prismjs/components/prism-javascript';
+  // import 'prismjs/components/prism-javascript';
   // import 'prismjs/components/prism-swift';
   // import 'prismjs/components/prism-typescript';
   // import 'prismjs/components/prism-yaml';
