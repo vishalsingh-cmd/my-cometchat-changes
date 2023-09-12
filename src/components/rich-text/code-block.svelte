21b5fe3275cee833ffc2054da9cf3978300cc5a7
Joao Gomes <joaogomes@significa.co>
2023-09-12 18:36:03 +0100

    feat(rich-text-code-block): add suport to use code-block component and apply the styles

---
+++ src/components/rich-text/code-block.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { ISbRichtext } from '@storyblok/js';
+  import Code from '$components/code-block/highlighted-code.svelte';
+  export let content: ISbRichtext;
+</script>
+
+{#if content.content && content?.content[0] && content?.content[0].text}
+  <div class="overflow-hidden rounded-2xl border border-gray-1" style="color-scheme: dark;">
+    <Code language={content?.attrs.class.split('language-')[1]} code={content?.content[0]?.text} />
+  </div>
+{/if}
