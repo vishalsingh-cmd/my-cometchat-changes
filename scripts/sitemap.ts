0d67644253d23f7fd981d5d94bbbe9ca56266463
Joao Gomes <joaogomes@significa.co>
2023-10-17 16:32:30 +0100

    fix(sitemap): caught main error

--- scripts/sitemap.ts
+++ scripts/sitemap.ts
@@ -112,4 +112,8 @@ async function main() {
   fs.writeFileSync('static/sitemap.xml', xml);
 }
 
-await main();
+try {
+  await main();
+} catch (error) {
+  console.error('Uncaught error on sitemap', error);
+}
