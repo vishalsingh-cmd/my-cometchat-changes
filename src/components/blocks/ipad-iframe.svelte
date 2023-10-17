0786771da055b6a2f3bf1cd4392820f62f9fe292
Joao Gomes <joaogomes@significa.co>
2023-10-17 17:33:09 +0100

    fix(ipad-iframe): limite width

--- src/components/blocks/ipad-iframe.svelte
+++ src/components/blocks/ipad-iframe.svelte
@@ -7,9 +7,9 @@
 </script>
 
 {#if block}
-  <section use:storyblokEditable={block} class="container mx-auto">
-    <div class="overflow-hidden px-container">
-      <div class="relative">
+  <section use:storyblokEditable={block} class="container mx-auto my-12">
+    <div class="flex items-center justify-center overflow-hidden px-container">
+      <div class="relative max-w-[1000px]">
         <!-- eslint-disable-next-line -->
         {@html block.iframe}
         <img src={Frame} class="h-full w-full" alt="IPad Frame" />
