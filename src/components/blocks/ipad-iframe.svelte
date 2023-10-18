52e4e4d4d72922b71eb4fdd471f8d4a9ddd50fb5
Joao Gomes <joaogomes@significa.co>
2023-10-18 15:48:30 +0100

    feat(ipad-iframe): increase tablet

--- src/components/blocks/ipad-iframe.svelte
+++ src/components/blocks/ipad-iframe.svelte
@@ -10,7 +10,7 @@
 {#if block}
   <section use:storyblokEditable={block} class="container mx-auto my-12">
     <div class="flex items-center justify-center overflow-hidden px-container">
-      <div class="relative max-w-[1000px]">
+      <div class="relative max-w-[1200px]">
         <!-- eslint-disable-next-line -->
         {@html block.iframe}
         <img src={Frame} class="h-full w-full" alt="IPad Frame" />
@@ -28,5 +28,14 @@
     height: calc(100% - 44px);
     width: calc(100% - 44px);
     border-radius: 12px;
+  }
+
+  @media only screen and (max-width: 600px) {
+    section :global(iframe) {
+      height: calc(100% - 24px);
+      width: calc(100% - 24px);
+      left: 12px;
+      top: 12px;
+    }
   }
 </style>
