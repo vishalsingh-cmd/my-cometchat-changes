ad1248398414eb2dfb04806e5c94b81a5c6b41d7
Joao Gomes <joaogomes@significa.co>
2023-11-22 10:23:38 +0000

    feat(structured-data-marktup): add support to both lp and normal pages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,18 @@
 module.exports = {
   root: true,
+  parser: '@typescript-eslint/parser',
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:svelte/recommended',
     'prettier',
     'plugin:storybook/recommended'
   ],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['svelte3', '@typescript-eslint'],
+  ignorePatterns: ['*.cjs'],
+  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  settings: {
+    'svelte3/typescript': () => require('typescript')
+  },
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2020,
@@ -19,16 +23,8 @@ module.exports = {
     es2017: true,
     node: true
   },
-  overrides: [
-    {
-      files: ['*.svelte'],
-      parser: 'svelte-eslint-parser',
-      parserOptions: {
-        parser: '@typescript-eslint/parser'
-      }
-    }
-  ],
   rules: {
+    // no-unused-vars
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': [
       'error',
