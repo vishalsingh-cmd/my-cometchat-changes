fb59ef96bdaeee8034e23473d16c9796ebc1c27b
SpikeySanju <spikeysanju98@gmail.com>
2024-02-29 08:48:04 +0530

    Update styles for static banners

--- src/components/center-static-banner-v01.svelte
+++ src/components/center-static-banner-v01.svelte
@@ -11,7 +11,7 @@
   <div
     use:storyblokEditable={block}
     class={cn(
-      'relative flex h-auto w-full flex-col gap-3 overflow-clip rounded-3xl bg-white p-8 shadow-md'
+      'relative flex h-auto w-full flex-col gap-3 overflow-clip rounded-3xl border border-gray-6 bg-[#F8F8F8] p-8 shadow-md'
     )}
   >
     <h1 class="text-xl font-semibold leading-tight">{block.title}</h1>
@@ -25,7 +25,7 @@
     {/if}
 
     {#if block.image}
-      <img src={block.image} alt="Comet Chat" class="absolute -right-24 top-24 w-96" />
+      <img src={block.image} alt="Comet Chat" class="absolute -right-36 top-24 w-96" />
     {/if}
   </div>
 {/if}
