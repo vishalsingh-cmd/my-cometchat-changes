2a8d5472fb9fef8a58aa61fac0525f5efe76252e
SpikeySanju <spikeysanju98@gmail.com>
2024-04-03 13:51:13 +0530

    Refactor pricing table header column component

--- src/components/pricing/enhanced/pricing-table-header-column-enhanced.svelte
+++ src/components/pricing/enhanced/pricing-table-header-column-enhanced.svelte
@@ -2,7 +2,7 @@
   import type { PricingTableHeaderEnhancedStoryblok } from '$types/bloks';
   import { storyblokEditable } from '$lib/actions/storyblok-editable';
 
-  export let price: number | null = null;
+  // export let price: number | null = null;
   export let column: PricingTableHeaderEnhancedStoryblok;
 </script>
 
@@ -13,14 +13,10 @@
     class="col-span-2 flex flex-col items-center justify-between gap-1.5 pb-8 pt-12 text-gray-12"
   >
     <p class="text-xl/tighter font-semibold">{title}</p>
-    <p class="text-lg/snug font-medium tracking-wide opacity-74">
-      {#if description}
+    {#if description}
+      <p class="text-lg/snug font-medium tracking-wide opacity-74">
         {description}
-      {:else if price === 0}
-        Free
-      {:else}
-        Starts at &#36;{price}
-      {/if}
-    </p>
+      </p>
+    {/if}
   </div>
 {/if}
