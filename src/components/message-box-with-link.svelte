b776d9888749d6e2dddebfd07e73507d5fa18e19
SpikeySanju <spikeysanju98@gmail.com>
2024-04-11 19:43:57 +0530

    Refactor message box with link component and update styling

--- src/components/message-box-with-link.svelte
+++ src/components/message-box-with-link.svelte
@@ -25,45 +25,47 @@
     class="absolute left-0 top-0 h-full w-12 rounded-l-xl bg-gradient-to-r from-brand-9/30 to-transparent"
   />
 
-  <div class="ml-6 w-fit items-center justify-center rounded-full bg-brand-5 p-3">
-    <Icon icon="triangle-warning" size="sm" class="h-6 w-6 text-brand-9" />
-  </div>
+  <div class="flex w-full flex-col gap-2">
+    <div class="ml-6 w-fit items-center justify-center rounded-full bg-brand-5 p-3">
+      <Icon icon="triangle-warning" size="sm" class="h-6 w-6 text-brand-9" />
+    </div>
 
-  <div class="ml-3 p-4">
-    {#if block.title && block.title.length > 0}
-      <div class="text-lg-richtext font-semibold text-brand-9">{block.title}</div>
-    {:else}
-      <div class="text-lg-richtext font-semibold text-brand-9">Important</div>
-    {/if}
+    <div class="ml-3 p-4">
+      {#if block.title && block.title.length > 0}
+        <div class="text-lg-richtext font-semibold text-brand-9">{block.title}</div>
+      {:else}
+        <div class="text-lg-richtext font-semibold text-brand-9">Important</div>
+      {/if}
 
-    {#if block.message && block.message.length > 0}
-      <p class="w-full whitespace-pre-line font-medium">{block.message}</p>
-    {:else}
-      <p class="w-full whitespace-pre-line font-medium">
-        This tutorial is outdated. We're currently working on an updated version. In the meantime,
-        for instructions on building a chat app using CometChat, please visit our documentation.
-      </p>
-    {/if}
+      {#if block.message && block.message.length > 0}
+        <p class="w-full whitespace-pre-line font-medium">{block.message}</p>
+      {:else}
+        <p class="w-full whitespace-pre-line font-medium">
+          This tutorial is outdated. We're currently working on an updated version. In the meantime,
+          for instructions on building a chat app using CometChat, please visit our documentation.
+        </p>
+      {/if}
 
-    {#if block.cta?.length > 0}
-      <div class="mt-3 flex gap-3">
-        {#each block.cta as button}
-          {@const { href, target, rel } = getAnchorFromCmsLink(button.link)}
-          <Button variant={'primary'} as="a" {href} {target} {rel}>
-            {button.label}
-          </Button>
-        {/each}
-      </div>
-    {:else}
-      <div class="mt-3 flex gap-3">
-        <Button
-          variant={'primary'}
-          href={'https://cometchat.com/docs/home'}
-          as="a"
-          target="_blank"
-          rel="noopener noreferrer">See our docs</Button
-        >
-      </div>
-    {/if}
+      {#if block.cta?.length > 0}
+        <div class="mt-3 flex gap-3">
+          {#each block.cta as button}
+            {@const { href, target, rel } = getAnchorFromCmsLink(button.link)}
+            <Button variant={'primary'} as="a" {href} {target} {rel}>
+              {button.label}
+            </Button>
+          {/each}
+        </div>
+      {:else}
+        <div class="mt-3 flex gap-3">
+          <Button
+            variant={'primary'}
+            href={'https://cometchat.com/docs/home'}
+            as="a"
+            target="_blank"
+            rel="noopener noreferrer">See our docs</Button
+          >
+        </div>
+      {/if}
+    </div>
   </div>
 </div>
