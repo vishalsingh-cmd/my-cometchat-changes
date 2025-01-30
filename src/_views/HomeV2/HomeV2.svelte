e8749498661764ee9c71de26bfcef3064efe1476
prakash03dev <prakash03.dev@gmail.com>
2025-01-30 19:44:08 +0530

    clients working

--- src/_views/HomeV2/HomeV2.svelte
+++ src/_views/HomeV2/HomeV2.svelte
@@ -2,9 +2,11 @@
   import Page from '$src/_comps/layouts/Page.svelte';
   import Banner from './_sections/Banner/Banner.svelte';
   import FeatureTabs from './_sections/FeatureTabs/FeatureTabs.svelte';
+  import Clients from './_sections/Clients/Clients.svelte';
 </script>
 
 <Page>
   <Banner />
   <FeatureTabs />
+  <Clients />
 </Page>
---
+++ src/_views/HomeV2/_sections/Clients/Clients.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Container from '$src/_comps/layouts/Container.svelte';
+  import Section from '$src/_comps/layouts/Section.svelte';
+</script>
+
+<Section className="relative isolate">
+  <Container pxEnabled={false}>
+    <div class="flex flex-col text-center" />
+  </Container>
+</Section>
