a3a1e2fffaa68ec745cca939872a6837d921edb1
kesav <kesav424@gmail.com>
2025-02-16 15:22:47 +0530

    error cleard

--- src/_views/Templates/Listing/Listing.svelte
+++ src/_views/Templates/Listing/Listing.svelte
@@ -8,8 +8,7 @@
   import ListingCard from './_comps/ListingCard.svelte';
   import Section from '$src/_comps/layouts/Section.svelte';
   import Container from '$src/_comps/layouts/Container.svelte';
-
-
+  import ListingCardSkeleton from './_comps/ListingCardSkeleton.svelte';
 
   export let block: templates_listingStoryblok;
   const { templates, actions } = createTemplatesContext({
@@ -33,27 +32,24 @@
     expand="full"
     className={cn(['flex flex-col gap-5 px-5 py-10'], ['lg:px-10'])}
   >
- 
-  <div class={cn(['grid grid-cols-1 gap-5'], ['lg:grid-cols-2 lg:gap-8'])}>
-    <!-- loader -->
-    {#if $templates.isLoading}
-      <ListingCardSkeleton />
-      <ListingCardSkeleton />
-      <ListingCardSkeleton />
-    {:else if $templates.data}
-      {#each $templates.data.stories as story}
-        {#if story && story.content.seo[0] && story.content.seo[0].og_image && story.name}
-          {@const storyLink = sanitizeSlug(story.full_slug)}
-          <ListingCard
-            image={story.content.seo[0].og_image}
-            title={story.name}
-            href={storyLink}
-          />
-        {/if}
-      {/each}
-    {/if}
-  </div>
-</div>
-
+    <div class={cn(['grid grid-cols-1 gap-5'], ['lg:grid-cols-2 lg:gap-8'])}>
+      <!-- loader -->
+      {#if $templates.isLoading}
+        <ListingCardSkeleton />
+        <ListingCardSkeleton />
+        <ListingCardSkeleton />
+      {:else if $templates.data}
+        {#each $templates.data.stories as story}
+          {#if story && story.content.seo[0] && story.content.seo[0].og_image && story.name}
+            {@const storyLink = sanitizeSlug(story.full_slug)}
+            <ListingCard
+              image={story.content.seo[0].og_image}
+              title={story.name}
+              href={storyLink}
+            />
+          {/if}
+        {/each}
+      {/if}
+    </div>
   </Container>
 </Section>
