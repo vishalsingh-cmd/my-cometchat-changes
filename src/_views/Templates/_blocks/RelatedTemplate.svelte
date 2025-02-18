121996671f91df88c05b67d90065a1aeb2d6cb5a
kesav <kesav424@gmail.com>
2025-02-18 16:03:20 +0530

    relate card done

--- src/_views/Templates/_blocks/RelatedTemplate.svelte
+++ src/_views/Templates/_blocks/RelatedTemplate.svelte
@@ -24,7 +24,7 @@
   <a
     href={sanitizeSlug($template.data.story.full_slug)}
     target="_blank"
-    class={cn(['grid max-w-[341px] grid-rows-[auto_1fr] gap-4'], ['lg:gap-5'])}
+    class={cn(['group/relateCard grid max-w-[341px] grid-rows-[auto_1fr] gap-4'], ['lg:gap-5'])}
   >
     <div class="relative h-auto w-full">
       <Media
@@ -32,14 +32,29 @@
         imageTransformOptions={{ size: [1200, 0] }}
         media={seo.og_image}
       />
+      <div
+        class={cn([
+          'absolute inset-0',
+          'flex items-center justify-center',
+          'rounded-[14px] bg-[#141414] bg-opacity-20 backdrop-blur-[10px]',
+          'transition-all duration-300',
+          'opacity-0 group-hover/relateCard:opacity-100'
+        ])}
+      >
+        <button
+          class={cn([
+            'bg-white font-inter font-[500] text-[#141414]',
+            'h-[40px] w-[160px] rounded-[8px]'
+          ])}
+        >
+          View More
+        </button>
+      </div>
     </div>
     <div class="flex flex-col gap-1">
       <TemplatesHeading className="line-clamp-1" tag="h3" size="h4" weight="500">
         {seo.title}
       </TemplatesHeading>
-      <TemplatesParah className="line-clamp-2" size="base">
-        {seo.description}
-      </TemplatesParah>
     </div>
   </a>
 {/if}
--- src/_views/Templates/_blocks/RelatedTemplateSkeleton.svelte
+++ src/_views/Templates/_blocks/RelatedTemplateSkeleton.svelte
@@ -2,12 +2,22 @@
   import { cn } from '$src/_utils/tailwind.utils';
 </script>
 
-<div class={cn(['grid w-full max-w-[341px] grid-rows-[auto_1fr] gap-4'], ['lg:gap-5'])}>
+<div class={cn(['grid w-full max-w-[341px] grid-rows-[auto_1fr] gap-2'], ['lg:gap-2'])}>
   <div class="relative h-auto w-full">
-    <div class="h-[174px] w-full animate-pulse rounded-2xl bg-[#b1b5be]" />
+    <div class="h-[174px] w-full animate-pulse rounded-2xl bg-[#b1b5be] lg:h-56" />
+    <svg
+      class="absolute left-1/2 top-1/2 h-10 w-10 -translate-x-1/2 -translate-y-1/2"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 20 18"
+    >
+      <path
+        d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"
+      />
+    </svg>
   </div>
   <div class="flex flex-col gap-1">
     <div class="h-[1.2em] w-4/5 animate-pulse rounded bg-[#b1b5be]" />
-    <div class="h-[2.2em] w-full animate-pulse rounded bg-[#b1b5be]" />
   </div>
 </div>
