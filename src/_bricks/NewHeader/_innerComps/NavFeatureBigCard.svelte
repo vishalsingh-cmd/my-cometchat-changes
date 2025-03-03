1f4671b8265e6d97f83578974aef9f5cf2f0a556
prakash03dev <prakash03.dev@gmail.com>
2025-03-03 19:07:00 +0530

    resources

--- src/_bricks/NewHeader/_innerComps/NavFeatureBigCard.svelte
+++ src/_bricks/NewHeader/_innerComps/NavFeatureBigCard.svelte
@@ -1,26 +1,41 @@
-<script>
+<script lang="ts">
   import { tv } from '$src/_utils/tailwind.utils';
-  import bgimg from '../_assets/guid-vard-img.png';
+  import { getAnchorFromCmsLink } from '$src/lib/storyblok';
+  import type { NavFeatureBigCardProps } from '../newHeader.types';
 
   export let className = '';
   export let imgClassName = '';
   export let infoClassName = '';
   export let titleClassName = '';
+  export let block: NavFeatureBigCardProps;
 
   const navFeatureBigCard = tv({
     slots: {
-      base: ['max-w-[210px]', 'border border-[#FAFAFF] border-opacity-5 rounded-md'],
-      image: ['w-full object-cover'],
+      base: [
+        'grid grid-rows-[auto_1fr]',
+        'group/navFeatureBigCard min-w-[210px] w-full',
+        'border border-[#FAFAFF] border-opacity-5 rounded-md',
+        'hover:border-[#6852D666]',
+        'transition-colors duration-300'
+      ],
+      image: ['w-full h-full max-h-[120px] object-cover object-bottom'],
       info: ['px-4 py-4 flex flex-col'],
-      title: ['font-sans text-[14px] font-semibold text-[#FAFAFF] text-opacity-60']
+      title: [
+        'font-sans text-[14px] font-semibold text-[#FAFAFF] text-opacity-60',
+        'group-hover/navFeatureBigCard:text-[#6852D6]',
+        'transition-colors duration-300'
+      ]
     }
   });
   const { base, image, info, title } = navFeatureBigCard();
+  const { href } = getAnchorFromCmsLink(block.link);
 </script>
 
-<a href="/" class={base({ class: className })}>
-  <img src={bgimg} alt="decoration" class={image({ class: imgClassName })} />
+<a href={href || '#'} target="_blank" class={base({ class: className })}>
+  <img src={block.image.filename} alt={block.title} class={image({ class: imgClassName })} />
   <div class={info({ class: infoClassName })}>
-    <h3 class={title({ class: titleClassName })}>CometChat for Marketplaces</h3>
+    <h3 class={title({ class: titleClassName })}>
+      {block.title}
+    </h3>
   </div>
 </a>
--- src/_bricks/NewHeader/_sectionbloks/NavFeatureBigCards.svelte
+++ src/_bricks/NewHeader/_sectionbloks/NavFeatureBigCards.svelte
@@ -1,5 +1,45 @@
 <script lang="ts">
   import type { NavFeatureBigCardsProps } from '../newHeader.types';
+  import { getAnchorFromCmsLink } from '$src/lib/storyblok';
+  import { tv } from '$src/_utils/tailwind.utils';
+  import NavSection from '../_innerComps/NavSection.svelte';
+  import NavTitle from '../_innerComps/NavTitle.svelte';
+  import NavFeatureBigCard from '../_innerComps/NavFeatureBigCard.svelte';
+  import GhostButton from '$src/components/buttons/ghost-button.svelte';
 
+  export let className = '';
+  export let cardsClassName = '';
   export let block: NavFeatureBigCardsProps;
+
+  const navFeatureBigCards = tv({
+    slots: {
+      base: ['flex flex-col gap-4'],
+      cards: ['flex gap-4 overflow-auto']
+    }
+  });
+  const { base, cards } = navFeatureBigCards();
 </script>
+
+<NavSection className={base({ class: className })}>
+  <NavTitle className="px-5">{block.title}</NavTitle>
+
+  <div class={cards({ class: cardsClassName })} data-scrollbar="hide">
+    <div class="w-5" />
+    {#each block.cards as card}
+      <NavFeatureBigCard block={card} />
+    {/each}
+    <NavFeatureBigCard className="min-w-max w-max hidden xl:grid" block={block.see_all_card[0]} />
+    <div class="w-5" />
+  </div>
+  {@const { href } = getAnchorFromCmsLink(block.see_all_card[0].link)}
+
+  <GhostButton
+    as={'a'}
+    href={href || '#'}
+    target="_blank"
+    variant="highlighted"
+    class={'block px-5 xl:hidden'}
+  >
+    See All
+  </GhostButton>
+</NavSection>
