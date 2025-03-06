045071ab0f1bed79a196f29c4aa00a3220b7ad3e
prakash03dev <prakash03.dev@gmail.com>
2025-03-06 16:04:27 +0530

    re

--- src/_views/HomeV2/_sections/Industry/_comps/IndustryRollerItem.svelte
+++ src/_views/HomeV2/_sections/Industry/_comps/IndustryRollerItem.svelte
@@ -30,6 +30,7 @@
   ])}
   data-state={status}
   on:click={handleOnClick}
+  on:keypress
 >
   <div class="grid grid-cols-[auto_1fr] gap-4 pb-10">
     <div
@@ -98,7 +99,7 @@
       'h-[1px] w-full -translate-x-full bg-[#FF7F3E]',
       'transition-transform duration-200',
       'group-data-[state="active"]/industryRollerItem:duration-[8000ms]',
-      'group-data-[anim="active"]/containerElem:group-data-[state="active"]:translate-x-0'
+      'group-data-[state="active"]/industryRollerItem:translate-x-0'
     ])}
   />
 </div>
