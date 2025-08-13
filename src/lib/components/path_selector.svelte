996642054faaa91ca065d6ea8cd3ccb70b62ccaa
HitanshuDev <hitanshu212003@gmail.com>
2025-08-13 08:27:53 +0530

    fix image name in path_selector comp

--- src/lib/components/path_selector.svelte
+++ src/lib/components/path_selector.svelte
@@ -1,7 +1,7 @@
 <script>
   let byobIcon = 'agent_lp_images/AI2_icon.png';
   let cometchatIcon = 'agent_lp_images/AI_icon.png';
-  let cometchatImage = 'agent_lp_images/Infra.png';
+  let cometchatImage = 'agent_lp_images/infra.png';
 
   let byobImage = '/agent_lp_images/Byob.png';
   let bulletIcon = '/agent_lp_images/bullet_icon.png';
