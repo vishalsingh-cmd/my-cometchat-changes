49c55dfabffef1926f0394126be36a878e49015f
HitanshuDev <hitanshu212003@gmail.com>
2025-09-17 17:30:20 +0530

    mod: core feature blok

--- src/components/blocks/core-features.svelte
+++ src/components/blocks/core-features.svelte
@@ -52,13 +52,13 @@
                 {#if block.subtitle}
                   <div class=" pb-2 text-3xl font-semibold leading-tighter">
                     {@html block.subtitle}
-                  </div>
-                {/if}
-                {#if block.gradient_title}
-                  <div
-                    class="bg-gradient-purple bg-clip-text pb-2 text-3xl font-semibold leading-tighter text-transparent"
-                  >
-                    {block.gradient_title}
+                    {#if block.gradient_title}
+                      <span
+                        class="bg-gradient-purple bg-clip-text pb-2 text-3xl font-semibold leading-tighter text-transparent"
+                      >
+                        {block.gradient_title}
+                      </span>
+                    {/if}
                   </div>
                 {/if}
               </div>
