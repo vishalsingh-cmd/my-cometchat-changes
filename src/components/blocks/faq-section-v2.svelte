b152c29dc2f19e1c11b63944694d51dfdce5ea7a
HitanshuDev <hitanshu212003@gmail.com>
2025-10-27 16:10:08 +0530

    fix: faq section fixing

--- src/components/blocks/faq-section-v2.svelte
+++ src/components/blocks/faq-section-v2.svelte
@@ -2,7 +2,8 @@
   import type { FaqSectionStoryblok } from '$types/bloks';
   import { cn, getLabelInfo } from '$lib/utils';
   import { storyblokEditable } from '$lib/actions/storyblok-editable';
-  import Title from '$components/title.svelte';
+  // import Title from '$components/title.svelte';
+  import { resolver } from '../rich-text/rich-text-renderer.svelte';
   import Icon from '$components/icon/icon.svelte';
   import Accordion from '$components/accordion.svelte';
   import AccordionGroup from '$components/accordion-group.svelte';
@@ -12,24 +13,57 @@
 </script>
 
 {#if block}
-  <section use:storyblokEditable={block} data-theme={block.theme} class="overflow-hidden bg-gray-1">
+  <section
+    use:storyblokEditable={block}
+    data-theme={block.theme}
+    class="mt-[41px] overflow-hidden bg-gray-1"
+  >
     <div class="container relative mx-auto flex flex-col items-center px-container">
       {#if block.title?.[0]}
-        {#if block.title && block.title[0]}
-          {@const title = block.title[0]}
-          {@const label = getLabelInfo(title.label, title.label_colour || 'brand')}
-          <Title
-            {label}
-            title={title.title}
-            description={title.description}
-            buttons={block.title?.[0].links}
-            class="w-full items-center p-0 text-start md:text-center"
-          />
-        {/if}
+        {@const title = block.title[0]}
+        {@const label = getLabelInfo(title.label, title.label_colour || 'brand')}
+
+        <div
+          class="flex w-full flex-col items-center p-0 px-container py-12 text-start md:text-center lg:pb-[48px] lg:pt-[100px]"
+        >
+          {#if label?.content}
+            <p
+              class={`mb-2 max-w-[528px] text-xl font-semibold leading-tighter md:mb-3 ${
+                label.color === 'orange' ? 'text-orange-9' : 'text-brand-9'
+              }`}
+            >
+              {label.content}
+            </p>
+          {/if}
+
+          {#if title.title}
+            <h2 class="max-w-[528px] text-3xl font-semibold leading-tighter text-gray-12">
+              {title.title}
+            </h2>
+          {/if}
+
+          {#if title.description}
+            {#if typeof title.description !== 'string' && title.description.content}
+              {#each title.description.content as content}
+                <p
+                  class="mt-4 max-w-[528px] text-xl font-medium leading-snug tracking-wide text-gray-12 opacity-[0.74]"
+                >
+                  {@html resolver.render(content)}
+                </p>
+              {/each}
+            {:else}
+              <p
+                class="mt-4 max-w-[528px] text-xl font-medium leading-snug tracking-wide text-gray-12 opacity-[0.74]"
+              >
+                {title.description}
+              </p>
+            {/if}
+          {/if}
+        </div>
       {/if}
 
       {#if block.faqs && block.faqs.length > 0}
-        <div class="mt-10 flex w-full max-w-[751px] flex-col flex-wrap gap-4 text-gray-12 md:mt-16">
+        <div class=" flex w-full max-w-[751px] flex-col flex-wrap gap-4 text-gray-12">
           <AccordionGroup>
             {#each block.faqs as faq, i}
               {@const { title, text, _uid } = faq}
