20e339163ad79b5735dff32cce50f4635c05c014
HitanshuDev <hitanshu212003@gmail.com>
2025-10-28 17:30:52 +0530

    fix: pricing-table-Q3Y24.svelte show logic

--- src/components/blocks/pricing-table-Q3Y24.svelte
+++ src/components/blocks/pricing-table-Q3Y24.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
   import { cn } from '$lib/utils';
-  import {
-    activateTable,
-    isBilledAnnualy,
-    maus,
-    pricingValues
-  } from '$lib/stores/pricing-stores-v2';
+  import { activateTable, isBilledAnnualy, maus, pricingValues } from '$lib/stores/pricing-stores';
   import { onMount, tick } from 'svelte';
   import { page } from '$app/stores';
   import PricingTableLineQ3Y24 from './pricing-table-line-Q3Y24/pricing-table-line-Q3Y24.svelte';
@@ -134,7 +129,7 @@
   });
 </script>
 
-{#if $activateTable === 1 || $activateTable === 2}
+{#if $activateTable}
   <div class="container mx-auto pb-[80px]">
     <TitleSection block={block.title[0]} />
     <div class="hidden px-container lg:block" id="pricing-table-Q3Y24">
