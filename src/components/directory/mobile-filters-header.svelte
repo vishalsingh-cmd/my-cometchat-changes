837498e46d1661fbb89104b8def41fdd2ad2651f
HitanshuDev <hitanshu212003@gmail.com>
2025-10-30 00:31:14 +0530

    fix:tutorials directory bg colour

--- src/components/directory/mobile-filters-header.svelte
+++ src/components/directory/mobile-filters-header.svelte
@@ -10,7 +10,7 @@
 
 <div class="relative flex h-[58px] w-full items-center lg:hidden">
   <button class="h-4 w-4" on:click={() => dispatch('toggleFiltersPanel')}>
-    <Icon icon="x-close" size="xs" />
+    <Icon icon="x-close" size="xs" class="text-gray-1 opacity-74" />
   </button>
   <p
     class="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform items-center gap-2 text-md font-semibold leading-tight tracking-wide text-gray-1 opacity-74"
