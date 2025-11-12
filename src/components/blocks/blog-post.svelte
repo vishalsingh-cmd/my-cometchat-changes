5a3ee2811ba309c2439bced9015cf51d5d1be38c
HitanshuDev <hitanshu212003@gmail.com>
2025-11-12 21:00:57 +0530

    edit: new blog design

--- src/components/blocks/blog-post.svelte
+++ src/components/blocks/blog-post.svelte
@@ -2,12 +2,18 @@
   import type { BlogPostStoryblok } from '$types/bloks';
   import Hero from '$components/blog-post/hero/hero.svelte';
   import PostLayout from '$components/post/post-layout.svelte';
+  import PostLayoutV2 from '../post/post-layout-v2.svelte';
 
   export let block: BlogPostStoryblok;
 </script>
 
-{#if block}
+<!-- {#if block}
   <PostLayout {block}>
     <Hero {block} />
   </PostLayout>
+{/if} -->
+{#if block}
+  <PostLayoutV2 {block}>
+    <Hero {block} />
+  </PostLayoutV2>
 {/if}
---
+++ src/components/rich-text-v2/heading.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { cn } from '$lib/utils';
+  import { RichTextResolver, type ISbRichtext } from '@storyblok/js';
+
+  export let content: ISbRichtext;
+
+  const resolver = new RichTextResolver();
+</script>
+
+<svelte:element
+  this={`h${content.attrs.level}`}
+  class={cn(
+    'font-semibold leading-tighter text-gray-12 [&_span]:!text-gray-1',
+    content?.attrs?.level == 2 && 'text-h1',
+    content?.attrs?.level == 2 && 'text-h2',
+    content?.attrs?.level == 3 && 'text-h3',
+    content?.attrs?.level == 4 && 'text-h4',
+    content?.attrs?.level == 2 ? 'mb-2.5 mt-5 md:mb-3 md:mt-10' : 'my-3 md:mb-3 md:mt-5'
+  )}
+>
+  <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+  {@html resolver.render(content)}
+</svelte:element>
